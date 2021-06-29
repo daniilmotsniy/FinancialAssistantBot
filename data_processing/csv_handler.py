fad39f9f4aa0a2e045be901aa1ca18edc0b3e103
Daniil Motsnyi <daniil_motsnyi@epam.com>
2021-06-29 21:02:04 +0300

    estimating assets for each type

--- data_processing/csv_handler.py
+++ data_processing/csv_handler.py
@@ -1,7 +1,20 @@
-# COPY users TO 'C:\Files\users.csv' DELIMITER ',' CSV HEADER;
+# COPY users TO 'C:\Files\test_users.csv' DELIMITER ',' CSV HEADER;
+import json
 
+import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('data.csv')
+raw_data = pd.read_csv('test_users.csv')
+raw_assets = raw_data['user_assets']
 
+frames = []
 
+for ass in raw_assets:
+    frames.append(pd.DataFrame.from_dict(json.loads(ass), orient='index').transpose())
+
+result = pd.concat(frames)
+
+# this plot shows count of assets by each type from all users
+result.count().plot()
+
+plt.show()
