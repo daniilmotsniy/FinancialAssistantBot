9674372c8ce30d2ea5b7f1c646ab90df164800ab
Daniil Motsnyi <daniil_motsnyi@epam.com>
2021-08-29 14:13:05 +0300

    Assets chart

--- analytics_ui/src/App.js
+++ analytics_ui/src/App.js
@@ -1,15 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
 import Asset from "./Asset";
+import Assets from "./Assets";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          <Asset asset='user_cryptos' color='rgb(0,192,0)'/>
-          <Asset asset='user_stocks' color='rgb(163,0,0)'/>
-          <Asset asset='user_currencies' color='rgb(163,233,0)'/>
+          <Asset asset='user_cryptos' color='rgb(209,167,29)'/>
+          <Asset asset='user_stocks' color='rgb(87,168,60)'/>
+          <Asset asset='user_currencies' color='rgb(80,60,168)'/>
+          <Assets />
       </header>
     </div>
   );
---
+++ analytics_ui/src/Assets.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import {Pie} from 'react-chartjs-2';
+import {gql, useQuery} from "@apollo/client";
+
+const ASSETS_COUNT = gql`
+  query AssetsCount {
+   assetsCount {
+     userStocks
+     userCurrencies
+     userCryptos
+     userResources
+   }
+  }
+`;
+
+function Assets() {
+    const { loading, error, data } = useQuery(ASSETS_COUNT)
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>${error}</p>
+
+    let state = {
+        labels: Object.keys(data.assetsCount),
+        datasets: [
+            {
+                label: 'Total count of assets',
+                backgroundColor: 'rgb(209,167,29)',
+                borderColor: 'rgb(70,70,70)',
+                borderWidth: 2,
+                data: Object.values(data.assetsCount)
+            }
+        ]
+    }
+
+    return (
+        <div>
+            <Pie
+                data={state}
+                options={{
+                    title:{
+                        display:true,
+                        fontSize:20
+                    },
+                    legend:{
+                        display:true,
+                        position:'right'
+                    }
+                }}
+            />
+        </div>
+    )
+}
+
+export default Assets
