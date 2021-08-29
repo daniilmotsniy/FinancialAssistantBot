10c6cd4ba0a7aea76b6ce0678d58f8f6ede60060
Daniil Motsnyi <daniil_motsnyi@epam.com>
2021-08-29 18:52:52 +0300

    Assets chart fix

--- analytics_ui/src/Assets.jsx
+++ analytics_ui/src/Assets.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Pie} from 'react-chartjs-2';
 import {gql, useQuery} from "@apollo/client";
+import {chartColors} from "./colors";
 
 const ASSETS_COUNT = gql`
   query AssetsCount {
@@ -24,7 +25,7 @@ function Assets() {
         datasets: [
             {
                 label: 'Total count of assets',
-                backgroundColor: 'rgb(209,167,29)',
+                backgroundColor: chartColors,
                 borderColor: 'rgb(70,70,70)',
                 borderWidth: 2,
                 data: Object.values(data.assetsCount)
